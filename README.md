f73b9ea57f96c73f606bb0713dd84006b5471943
fcamargo3 <fcamargo3@gatech.edu>
2021-08-15 01:03:40 -0500

    Starting to add unittest code.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gh_vscode
 Test Github's use of VS code.
+
+## unittest
+This works from parent directory
+python -m unittest tests.test_a
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+def my_sum(a,b):
+    return a + b
+
 def main():
     print('Hello World.')
+    x = 'a'
+    y = 'b'
 
 if __name__ == "__main__":
     main()
