5f00006520e3c183fa13fecc3407860594b0ec54
fcamargo3 <fcamargo3@gatech.edu>
2021-08-15 15:04:01 -0500

    Added pytest example that handles a TypeError.

--- tests/test_c.py
+++ tests/test_c.py
@@ -5,13 +5,22 @@ Created on Sun Aug 15 14:23:04 2021
 @author: camar
 """
 
+# https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest
+
+import sys
+if '..' not in sys.path:
+    sys.path.append('..')
+
+from src import main
+
 import pytest
 
-def capital_case(x):
-    return x.capitalize()
-
 def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
-    
+    assert main.capital_case('semaphore') == 'Semaphore'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        main.capital_case(9)
+        
 if __name__ == '__main__':
     pytest.main()
